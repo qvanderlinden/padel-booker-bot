66064dc6a6afaf8e9065289b65c6434dfb03346c
Quentin Vanderlinden <quentin.vanderlinden@gmail.com>
2021-06-04 23:07:43 +0200

    feat: login

--- src/booking-bot/index.ts
+++ src/booking-bot/index.ts
@@ -1,1 +1,51 @@
-console.log("here")
+import * as puppeteer from 'puppeteer';
+
+const HOMEPAGE = 'https://www15.iclub.be/myiclub.asp?ClubID=612';
+const CREDENTIALS = {
+  username: 'quentin.vanderlinden@gmail.com',
+  password: 'LAnDoUntqvNiNh4sitH9',
+};
+
+const notify = (e: Error) => {
+	console.error(e)
+}
+
+const main = async () => {
+	try {
+		// navigate to page
+		const browser = await puppeteer.launch({
+			defaultViewport: {
+				width: 1600,
+				height: 800,
+			},
+			headless: false
+		});
+		const page = await browser.newPage();
+		await page.goto(HOMEPAGE);
+	
+		// insert credentials
+		const usernameInput = await page.$("[name=Login]")
+		if (!usernameInput) {
+			throw new Error("Cannot find username form input")
+		}
+		await usernameInput.type(CREDENTIALS.username)
+		
+		const passwordInput = await page.$("[name=Password]")
+		if (!passwordInput) {
+			throw new Error("Cannot find password form input")
+		}
+		await passwordInput.type(CREDENTIALS.password)
+
+		// submit form
+		const loginForm = await page.$("[id=SignIn]")
+		if (!loginForm) {
+			throw new Error("Cannot find the form to submit")
+		}
+		await loginForm.evaluate(form => (form as HTMLFormElement).submit())
+
+	} catch (e) {
+		notify(e)
+	}
+};
+
+main();
